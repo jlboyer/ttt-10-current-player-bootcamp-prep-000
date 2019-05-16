c1a4dc626112bf3ac6b99abf8aabda92b2d5ddd8
mathematical-algorithm-3478 <jlboyer1@gmail.com>
2019-05-16 20:18:11 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,16 @@
 def turn_count(board)
   count = 0
   board.each do |value|
-    if value == "X" 
+    if value == "X" || value == "O"
+      count += 1
+    end
+  end
+end
+
+def current_player(board)
+  if turn_count % 2 == 0 
+    "O"
+  else
+    "X"
+  end
+end
